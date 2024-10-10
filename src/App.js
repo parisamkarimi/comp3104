865378cb46d525060c551ce8c39a152b9bb04e51
parisa_mkarimi <parisamkariimii@gmail.com>
2024-10-10 12:53:36 -0400

    Set up GitHub Actions for deployment

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <div className="App">
       <h1>Student Information System</h1>
-      <h5>Developed By: 101449727 || Parisa Mohammadkarimi || DevOps</h5>
+      <h5>Developed By: 101449727 || Parisa Mohammadkarimi || DevOps </h5>
       <StudentForm addStudent={addStudent} />
       <StudentList students={students} />
     </div>
